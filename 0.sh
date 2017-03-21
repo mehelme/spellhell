69593c042bb4d12acd7941ab0d636cf3c62adb1c
aaa422 <aaa422@linux-ik1n.suse>
2017-03-21 10:13:54 +0300

    my gcc6 call and moc

---
+++ 0.sh
@@ -0,0 +1,1 @@
+g++-6 -c -pipe -O2 -std=gnu++0x -Wall -W -D_REENTRANT -fPIC -DQT_NO_DEBUG -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB -fplugin=./spellhell.so -I. -isystem /usr/include/qt5 -isystem /usr/include/qt5/QtWidgets -isystem /usr/include/qt5/QtGui -isystem /usr/include/qt5/QtCore -I. -I/usr/lib64/qt5/mkspecs/linux-g++ -o moc_calculator.o moc_calculator.cpp
