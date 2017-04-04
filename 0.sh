6b994c7d74c20cf307c1171388827f33e9e0ad54
aaa422 <aaa422@linux-ik1n.suse>
2017-04-04 12:30:33 +0300

    issue comment

--- 0.sh
+++ 0.sh
@@ -1,1 +1,4 @@
-g++-6 -c -pipe -O2 -std=gnu++0x -Wall -W -D_REENTRANT -fPIC -DQT_NO_DEBUG -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB -fplugin=./spellhell.so -I. -isystem /usr/include/qt5 -isystem /usr/include/qt5/QtWidgets -isystem /usr/include/qt5/QtGui -isystem /usr/include/qt5/QtCore -I. -I/usr/lib64/qt5/mkspecs/linux-g++ -o moc_calculator.o moc_calculator.cpp
+rm  calculator.o
+g++-6 -c -pipe -O2 -std=gnu++0x -Wall -W -D_REENTRANT -fPIC -DQT_NO_DEBUG -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB -fplugin=./spellhell.so -I. -isystem /usr/include/qt5 -isystem /usr/include/qt5/QtWidgets -isystem /usr/include/qt5/QtGui -isystem /usr/include/qt5/QtCore -I. -I/usr/lib64/qt5/mkspecs/linux-g++ \
+-o calculator.o calculator.cpp 2>&1 | tee calculator_build_log.txt
+
